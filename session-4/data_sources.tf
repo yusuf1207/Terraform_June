8306bcb3c1c20d4a49ae9d224abb22f6a82293df
yusuf1207 <mukhammadturgunov@gmail.com>
2022-09-27 10:30:55 -0500

    session-4 modified

---
+++ session-4/data_sources.tf
@@ -0,0 +1,15 @@
+data "aws_ami" "amazon_linux_2" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
