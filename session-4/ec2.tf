9e031dcfb3618d12719ffb9a345a9011b17c015d
yusuf1207 <mukhammadturgunov@gmail.com>
2022-09-27 11:36:07 -0500

    -app-instance is added to Name

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -2,8 +2,10 @@ resource "aws_instance" "my_ec2" {
   ami           = data.aws_ami.amazon_linux_2.image_id
   instance_type = var.instance_type
   tags = {
-    Name = var.env
+    Name = var.env-app-instance
   }
   vpc_security_group_ids = [aws_security_group.my_sg.id]
 }
 
+# Combine static and dynamic values togather
+# Name = dev-app-instance, qa-app-instance, prod-app-instance
