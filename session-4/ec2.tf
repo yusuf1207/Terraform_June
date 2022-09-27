e750ced071ba823e28fbe819bed0fe229b3c5c80
yusuf1207 <mukhammadturgunov@gmail.com>
2022-09-27 11:43:03 -0500

    updated

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "my_ec2" {
   ami           = data.aws_ami.amazon_linux_2.image_id
   instance_type = var.instance_type
   tags = {
-    Name = var.env-app-instance
+    Name = "var.env-app-instance"
   }
   vpc_security_group_ids = [aws_security_group.my_sg.id]
 }
