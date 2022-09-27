463624e78fb9663268d0fa9fb2b3d0af5b54da31
yusuf1207 <mukhammadturgunov@gmail.com>
2022-09-27 14:53:30 -0500

    function is added

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -3,9 +3,13 @@ resource "aws_instance" "my_ec2" {
   instance_type = var.instance_type
   tags = {
     Name = "${var.env}-app-instance"
+    Name2 = format("%s-app-instance", var.env)
   }
   vpc_security_group_ids = [aws_security_group.my_sg.id]
 }
 
 # Combine static and dynamic values togather
 # Name = dev-app-instance, qa-app-instance, prod-app-instance
+
+# %s = string 
+# %d = number 
