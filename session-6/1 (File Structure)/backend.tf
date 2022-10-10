5585c7e94427f47de4e3e77fd6d5d08478b6e30f
yusuf1207 <mukhammadturgunov@gmail.com>
2022-10-10 14:27:05 +0000

    backend key changed form qa to dev

--- session-6/1 (File Structure)/backend.tf
+++ session-6/1 (File Structure)/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-2022-backend-do-not-delete"
-    key            = "session-6/qa/terraform.tfstate"
+    key            = "session-6/dev/terraform.tfstate"
     region         = "us-west-1"
     dynamodb_table = "terraform-2022-lock"
   }
