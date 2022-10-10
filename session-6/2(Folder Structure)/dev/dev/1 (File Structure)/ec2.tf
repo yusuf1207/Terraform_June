525815bf323155698b35579f9b67ab7d09da5129
yusuf1207 <mukhammadturgunov@gmail.com>
2022-10-10 15:19:04 +0000

    2(Folder Structure) added

---
+++ session-6/2(Folder Structure)/dev/dev/1 (File Structure)/ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "web" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  tags = {
+    name = format("%s-instance", var.env) # dev-instance
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = format("%s-web-sg", var.env)
+  description = "This is a secutiry group for my web instance"
+}
+
+resource "aws_security_group_rule" "web_sg_rule" {
+  count             = length(var.web_sg_ports)
+  type              = "ingress"
+  from_port         = element(var.web_sg_ports, count.index)
+  to_port           = element(var.web_sg_ports, count.index)
+  protocol          = "tcp"
+  cidr_blocks       = [element(var.web_sg_ports_cidr, count.index)]
+  security_group_id = aws_security_group.web_sg.id
+}
+
+resource "aws_security_group_rule" "web_sg_rule_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_sg.id
+}
---
+++ session-6/2(Folder Structure)/dev/dev/1 (File Structure)/variables.tf
@@ -0,0 +1,30 @@
+variable "ami" {
+  type        = string
+  description = "This is an AMI for my web instance"
+  default     = "ami-09208e69ff3feb1db"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "This is an instance type for web instance"
+  default     = "t2.micro"
+
+}
+
+variable "env" {
+  type        = string
+  description = "This varaibles represents the environment"
+  default     = "dev"
+}
+
+variable "web_sg_ports" {
+  type        = list(string)
+  description = "These are the ports for my security group"
+  default     = ["22", "80"]
+}
+
+variable "web_sg_ports_cidr" {
+  type        = list(string)
+  description = "These are the cidr_blocks for my ports"
+  default     = ["0.0.0.0/0", "0.0.0.0/0"]
+}
