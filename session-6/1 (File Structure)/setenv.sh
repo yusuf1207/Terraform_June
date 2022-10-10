6e63102f823d2006db43edce66d6c91a5ae16d59
yusuf1207 <mukhammadturgunov@gmail.com>
2022-10-10 14:53:20 +0000

    setenv.sh is updated

--- session-6/1 (File Structure)/setenv.sh
+++ session-6/1 (File Structure)/setenv.sh
@@ -2,12 +2,12 @@
 
 sudo rm -rf .terraform*
 #ENV = "$1"
-sed -i ' ' -e "s|_env_|ENV|" backend.tf 
+sed -i ' ' -e "s|_env_|$ENV|" backend.tf 
 terraform init
 echo "Environment is set to $ENV"
 terraform plan -var-file=tfvars/$ENV.tf
 terraform apply -var-file=tfvars/$ENV.tf -auto-approve
-sed -i ' ' -e "s|ENV|_env_" backend.tf
+sed -i ' ' -e "s|$ENV|_env_" backend.tf
 
 
 # sed and awk commands
