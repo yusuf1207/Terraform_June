9cca38acc11074c48f7ed626e91e0284222a3256
yusuf1207 <mukhammadturgunov@gmail.com>
2022-10-26 14:20:17 +0000

    variable.tf updated

--- session-7/backend/variable.tf
+++ session-7/backend/variable.tf
@@ -1,5 +1,5 @@
 variable "env" {
     type = string
-    descriprion = "This variable represents the environment such as dev, qa, and prod"
+    description = "This variable represents the environment such as dev, qa, and prod"
     default = "dev"
 }
