1f3f7b4cef0bc4013662b8bca6f242a9ddbbe8c3
yusuf1207 <mukhammadturgunov@gmail.com>
2022-11-08 16:46:01 +0000

    data_sources.rf added

--- session-7/backend/data_sources.tf
+++ session-7/backend/data_sources.tf
@@ -1,2 +1,15 @@
 # This feature is called data sourcing the remote backend
 # Data Source only fetches the data from outputs
+
+date "terraform_remote_state" "backend" {
+    backend = "s3"
+    config = {
+        bucket         = "terraform-2022-backend-do-not-delete"
+        key            = "session-7/backend/terraform.tfstate"
+        region         = "us-west-1"
+    }
+}
+
+output "password" {
+    value = data.terraform_remote_state.backend.outputs.db_password
+}
