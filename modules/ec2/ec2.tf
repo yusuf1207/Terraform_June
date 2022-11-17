bf3301d25d7c470074b20b7a5f4c91b6755d3f48
yusuf1207 <mukhammadturgunov@gmail.com>
2022-11-17 17:37:42 +0000

    ec2.tf updated

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,3 +1,8 @@
 resource "aws_instance" "main" {
     ami = var.ami
-}
+    instance_type = var.instance_type
+    tags = {
+        Name = "${var.env}-instance"
+        Environment = var.env
+    }
+} 
