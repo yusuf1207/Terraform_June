afbd3d4cbe87492d87f25165e6171fab87ab1a50
yusuf1207 <mukhammadturgunov@gmail.com>
2022-11-17 17:55:00 +0000

    session-9 and child modules are added

---
+++ session-9/qa/main.tf
@@ -0,0 +1,9 @@
+# Terraform Local module whtich consists of Child and Root modules
+
+module "ec2" {
+    source = "../../modules/ec2"   #Local or Remote, when it is a Local module, you provide a path of the Child module
+    # You can declare your variables as a configuration
+    ami = "ami-02538f8925e3aa27a"
+    instance_type = "t2.micro"
+    env = "qa"
+}
