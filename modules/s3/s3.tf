d27d1def1adbfe31d5a19988bd4aa85fc4b44544
yusuf1207 <mukhammadturgunov@gmail.com>
2022-11-18 16:56:43 +0000

    s3 child module is created

---
+++ modules/s3/s3.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "main" {
+    bucket = "${var.env}-terraform-module-test-yusuf"
+    acl = "private"
+}
---
+++ modules/s3/variables.tf
@@ -0,0 +1,5 @@
+variable "env" {
+    type = string
+    description = "This represents the environment"
+  
+}
