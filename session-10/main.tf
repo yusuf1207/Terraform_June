f9a53507b61ae09dae76a7a2e4d5e0d124c0c9df
yusuf1207 <mukhammadturgunov@gmail.com>
2022-11-21 15:09:51 +0000

    variables are added to Module

--- session-10/main.tf
+++ session-10/main.tf
@@ -2,4 +2,20 @@
 module "vpc" {
     source  = "terraform-aws-modules/vpc/aws"
     version = "3.14.0"
+
+// Variables
+  name = "aws-session-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-1a", "us-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
